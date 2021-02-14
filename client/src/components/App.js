7e34fdfb9da8e89f9bfcba983b462aa92fc45547
Tony Watkins <tony@llifion.net>
2021-02-14 10:37:27 +0000

    Use the link component and replace normal anchor tags with the link component; note also replace the href with to. With this in place, the app is no longer re-loaded

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,9 +4,7 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 const PageOne = () => {
   return (
       <div>PageOne
-        // BAD! Do not add an anchor tag to navigate to another page as you would normally.
-        // And really really bad - you lose all the state and data so far
-        <a href="pagetwo">Navigate to page two</a>)
+        <Link to="/pagetwo">Navigate to page two</Link>
       </div>
   )
 };
@@ -15,6 +13,7 @@ const PageTwo = () => {
     return <div>
         PageTwo
         <button>Click me</button>
+        <Link to="/">Navigate to page one</Link>
     </div>
 };
 
