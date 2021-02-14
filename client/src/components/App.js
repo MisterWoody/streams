dcdf34f0ad7a060ca075bd68e52b1ae0846e4850
Tony Watkins <tony@llifion.net>
2021-02-14 14:18:03 +0000

    Remove the previous testing components for route testing and import our new streaming components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-const PageOne = () => {
-  return (
-      <div>PageOne
-        <Link to="/pagetwo">Navigate to page two</Link>
-      </div>
-  )
-};
-
-const PageTwo = () => {
-    return <div>
-        PageTwo
-        <button>Click me</button>
-        <Link to="/">Navigate to page one</Link>
-    </div>
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />
-                    <Route path="/pagetwo" component={PageTwo} />
+                    
                 </div>
             </BrowserRouter>
         </div>
