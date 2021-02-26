12870c435afca638507f2bdef1341e6461da5b91
Tony Watkins <tony@llifion.net>
2021-02-26 11:03:24 +0000

    Add in a call to the google api to load the oauth2 client library javascript and on successful load initialise the client with our generated client Id

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+
+class GoogleAuth extends React.Component {
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '167103438212-bobl72astgh0il3mpokqrtp81ui523i0.apps.googleusercontent.com',
+                scope: 'email'
+            });
+        });
+    }
+    
+    render() {
+        return <div>Google Auth</div>
+    }
+}
+
+export default GoogleAuth;
