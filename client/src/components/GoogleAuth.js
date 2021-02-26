5b94430279b7fbc6ca500e9cbdfb9b891b2712a0
Tony Watkins <tony@llifion.net>
2021-02-26 15:09:58 +0000

    Remove the transient state message shown when we haven't yet received a message from Google out the signed in status of a user

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -22,7 +22,7 @@ class GoogleAuth extends React.Component {
     
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
-            return <div>I don't know if we are signed in</div>
+            return null;
         } else if (this.state.isSignedIn) {
             return <div>I am signed in!</div>
         } else {
